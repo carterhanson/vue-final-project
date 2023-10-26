c70740348b043b543a7e9cf53f1b1598ce2eb434
Carter Hanson <hansonc23@students.westerntc.edu>
2023-10-26 12:01:38 -0500

    class

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "./assets/main.css"
+
+
 createApp(App).use(router).mount('#app')
